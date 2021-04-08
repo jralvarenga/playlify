93a3e75a3a0c6347c26d35358907d9f6896fa798
jralvarenga <jralvarenga161@gmail.com>
2021-04-07 21:57:25 -0600

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   env: {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     REDIRECT_URI: process.env.REDIRECT_URI,
     CLIENT_URI: process.env.CLIENT_URI,
     WEB_PLAYBACK_SDK: process.env.WEB_PLAYBACK_SDK
+  },
+  pwa: {
+    dest: 'public'
   }
-}
+})

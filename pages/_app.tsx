dc79cd41779f2682964df2ec1e744e4a0cec23d9
jralvarenga <jralvarenga161@gmail.com>
2021-04-19 22:14:08 -0600

    New metas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,42 +8,39 @@ const MyApp = ({ Component, pageProps }: any) => {
   return (
     <>
       <Head>
+        {/* Primary Metas */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+
+        <title>Playlify - New Playlists with one song</title>
+
+        <meta name="title" content="Playlify - New Playlists with one song" />
         <meta name="description" content="Create new playlists and discover new music from just one song" />
         <meta name="keywords" content="spotify,playlist,song,create,podcast" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="theme-color" content="#191414" />
 
-        <meta name="description" content="Create new playlists and discover new music from just one song" />
-        <meta property="og:title" content="Playlify" />
-        <meta property="og:description" content="Create new playlists and discover new music from just one song" />
+        {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://playlify.vercel.app" />
-        <meta property="og:site_name" content="Playlify" />
+        <meta property="og:url" content="https://playlify.vercel.app/" />
+        <meta property="og:title" content="Playlify - New Playlists with one song" />
+        <meta property="og:description" content="Create new playlists and discover new music from just one song" />
         <meta property="og:image" content="/icons/icon-512.png" />
-        <meta property="og:image:width" content="512" />
-        <meta property="og:image:height" content="512" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:locale" content="en_US" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://playlify.vercel.app" />
-        <meta name="twitter:site" content="@rigo_alvarenga1" />
-        <meta name="twitter:creator" content="@rigo_alvarenga1" />
-        <meta name="twitter:title" content="Playlify" />
-        <meta name="twitter:description" content="Create new playlists and discover new music from just one song" />
-        <meta name="twitter:image" content="/icons/icon-512.png" />
-        <meta name="twitter:image:alt" content="Playlify" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://playlify.vercel.app/" />
+        <meta property="twitter:title" content="Playlify - New Playlists with one song" />
+        <meta property="twitter:description" content="Create new playlists and discover new music from just one song" />
+        <meta property="twitter:image" content="/icons/icon-512.png" />
 
-        <title>Playlify</title>
-
+        {/* Manifest */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
         <link rel="manifest" href="manifest.json"/>
         <link rel="stylesheet" href="global.css"/>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#191414" />
       </Head>
       <AuthProvider>
         <ThemeProvider theme={theme}>
